30d495ecd303acbf269611534b64c952efb98252
vrk12 <vrk12@case.edu>
2018-03-05 17:32:58 -0500

    added action file and updated server and client code

--- src/my_action_client.cpp
+++ src/my_action_client.cpp
@@ -12,7 +12,9 @@
 
 #include<ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
-#include<example_action_server/demoAction.h> //reference action message in this package
+#include<myAction.h> //reference action message in this package
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
 
 using namespace std;
 
@@ -23,7 +25,7 @@ int g_fdbk = -1;
 // This function will be called once when the goal completes
 // this is optional, but it is a convenient way to get access to the "result" message sent by the server
 void doneCb(const actionlib::SimpleClientGoalState& state,
-        const example_action_server::demoResultConstPtr& result) {
+        const my_action_server::myResultConstPtr& result) {
     ROS_INFO(" doneCb: server responded with state [%s]", state.toString().c_str());
     ROS_INFO("got result output = %d",result->output);
     g_result_output= result->output;
@@ -32,7 +34,7 @@ void doneCb(const actionlib::SimpleClientGoalState& state,
 
 //this function wakes up every time the action server has feedback updates for this client
 // only the client that sent the current goal will get feedback info from the action server
-void feedbackCb(const example_action_server::demoFeedbackConstPtr& fdbk_msg) {
+void feedbackCb(const my_action_server::myFeedbackConstPtr& fdbk_msg) {
     ROS_INFO("feedback status = %d",fdbk_msg->fdbk);
     g_fdbk = fdbk_msg->fdbk; //make status available to "main()"
 }
@@ -49,11 +51,11 @@ int main(int argc, char** argv) {
         ros::NodeHandle n;
         ros::Rate main_timer(1.0);
         // here is a "goal" object compatible with the server, as defined in example_action_server/action
-        example_action_server::demoGoal goal; 
+        my_action_server::demoGoal goal; 
         
         // use the name of our server, which is: timer_action (named in example_action_server_w_fdbk.cpp)
         // the "true" argument says that we want our new client to run as a separate thread (a good idea)
-        actionlib::SimpleActionClient<example_action_server::demoAction> action_client("my_action", true);
+        actionlib::SimpleActionClient<my_action_server::demoAction> action_client("my_action", true);
         
         // attempt to connect to the server: need to put a test here, since client might launch before server
         ROS_INFO("attempting to connect to server: ");
@@ -65,34 +67,61 @@ int main(int argc, char** argv) {
             server_exists = action_client.waitForServer(ros::Duration(1.0)); // retry every 1 second
         }
         ROS_INFO("connected to action server");  // if here, then we connected to the server;
+
+        geometry_msgs::PoseStamped pose_stamped;
+        geometry_msgs::Pose pose;
+        pose.position.x = 1.0; // say desired x-coord is 1
+        pose.position.y = 0.0;
+        pose.position.z = 0.0; // let's hope so!
+        pose.orientation.x = 0.0; //always, for motion in horizontal plane
+        pose.orientation.y = 0.0; // ditto
+        pose.orientation.z = 0.0; // implies oriented at yaw=0, i.e. along x axis
+        pose.orientation.w = 1.0; //sum of squares of all components of unit quaternion is 1
+        pose_stamped.pose = pose;
+        goal.input = pose_stamped; //move right
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
         
-        int countdown_goal = 1; //user will specify a timer value
-        while(countdown_goal>=0 && ros::ok()) {
-           cout<<"enter a desired timer value, in seconds (0 to abort, <0 to quit): ";
-           cin>>countdown_goal;
-           if (countdown_goal==0) { //see if user wants to cancel current goal
-             ROS_INFO("cancelling goal");
-             action_client.cancelGoal(); //this is how one can cancel a goal in process
-           }
-           if (countdown_goal<0) { //option for user to shut down this client
-              ROS_INFO("this client is quitting");
-              return 0;
-           }
-           //if here, then we want to send a new timer goal to the action server
-           ROS_INFO("sending timer goal= %d seconds to timer action server",countdown_goal);
-           goal.input = countdown_goal; //populate a goal message
-           //here are some options:
-           //action_client.sendGoal(goal); // simple example--send goal, but do not specify callbacks
-           //action_client.sendGoal(goal,&doneCb); // send goal and specify a callback function
-           //or, send goal and specify callbacks for "done", "active" and "feedback"
-           action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb); 
-           
-           //this example will loop back to the the prompt for user input.  The main function will be
-           // suspended while waiting on user input, but the callbacks will still be alive
-           //if user enters a new goal value before the prior request is completed, the prior goal will
-           // be aborted and the new goal will be installed
+        quat = convertPlanarPhi2Quaternion(0); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.x=3.5; 
+        goal.input = pose_stamped; //move right again
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
         
-       }
+        quat = convertPlanarPhi2Quaternion(1.57); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.y = 3.5;
+        goal.input = pose_stamped; //spin and move up
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+
+        quat = convertPlanarPhi2Quaternion(0); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.x = 7.5;
+        goal.input = pose_stamped; //move right again
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+
+        quat = convertPlanarPhi2Quaternion(1.56); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.y = 5.8;
+        goal.input = pose_stamped; //spin and move up
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+
+        quat = convertPlanarPhi2Quaternion(3.14); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.x = 3.5;
+        goal.input = pose_stamped; //spin and move up
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+
+        quat = convertPlanarPhi2Quaternion(1.57); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.y = 13.0;
+        goal.input = pose_stamped; //spin and move up
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
+
+        quat = convertPlanarPhi2Quaternion(3.14); // get a quaternion corresponding to this heading
+        pose_stamped.pose.orientation = quat;   
+        pose_stamped.pose.position.x = 1.0;
+        goal.input = pose_stamped; //spin and move up
+        action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
     return 0;
 }
 
--- src/my_action_server.cpp
+++ src/my_action_server.cpp
@@ -14,17 +14,113 @@
 
 #include<ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
 
 //the following #include refers to the "action" message defined for this package
 // The action message can be found in: .../example_action_server/action/demo.action
 // Automated header generation creates multiple headers for message I/O
 // These are referred to by the root name (demo) and appended name (Action)
-#include<example_action_server/demoAction.h>
+#include<my_action_server/myAction.h>
 
 int g_count = 0;
 bool g_count_failure = false;
+//some tunable constants, global
+const double g_move_speed = 1.0; // set forward speed to this value, e.g. 1m/s
+const double g_spin_speed = 1.0; // set yaw rate to this value, e.g. 1 rad/s
+const double g_sample_dt = 0.01;
+const double g_dist_tol = 0.001; // 1cm
+//global variables, including a publisher object
+geometry_msgs::Twist g_twist_cmd;
+ros::Publisher g_twist_commander; //global publisher object
+geometry_msgs::Pose g_current_pose; // not really true--should get this from odom 
 
-class ExampleActionServer {
+
+// here are a few useful utility functions:
+double sgn(double x);
+double min_spin(double spin_angle);
+double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion);
+geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi);
+
+void do_halt();
+void do_move(double distance);
+void do_spin(double spin_ang);
+
+//signum function: strip off and return the sign of the argument
+double sgn(double x) { if (x>0.0) {return 1.0; }
+    else if (x<0.0) {return -1.0;}
+    else {return 0.0;}
+}
+
+//a function to consider periodicity and find min delta angle
+double min_spin(double spin_angle) {
+        if (spin_angle>M_PI) {
+            spin_angle -= 2.0*M_PI;}
+        if (spin_angle< -M_PI) {
+            spin_angle += 2.0*M_PI;}
+         return spin_angle;   
+}            
+
+// a useful conversion function: from quaternion to yaw
+double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion) {
+    double quat_z = quaternion.z;
+    double quat_w = quaternion.w;
+    double phi = 2.0 * atan2(quat_z, quat_w); // cheap conversion from quaternion to heading for planar motion
+    return phi;
+}
+
+//and the other direction:
+geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi) {
+    geometry_msgs::Quaternion quaternion;
+    quaternion.x = 0.0;
+    quaternion.y = 0.0;
+    quaternion.z = sin(phi / 2.0);
+    quaternion.w = cos(phi / 2.0);
+    return quaternion;
+}
+
+// a few action functions:
+//a function to reorient by a specified angle (in radians), then halt
+void do_spin(double spin_ang) {
+    ros::Rate loop_timer(1/g_sample_dt);
+    double timer=0.0;
+    double final_time = fabs(spin_ang)/g_spin_speed;
+    g_twist_cmd.angular.z= sgn(spin_ang)*g_spin_speed;
+    while(timer<final_time) {
+          g_twist_commander.publish(g_twist_cmd);
+          timer+=g_sample_dt;
+          loop_timer.sleep(); 
+          }  
+    do_halt(); 
+}
+
+//a function to move forward by a specified distance (in meters), then halt
+void do_move(double distance) { // always assumes robot is already oriented properly
+                                // but allow for negative distance to mean move backwards
+    ros::Rate loop_timer(1/g_sample_dt);
+    double timer=0.0;
+    double final_time = fabs(distance)/g_move_speed;
+    g_twist_cmd.angular.z = 0.0; //stop spinning
+    g_twist_cmd.linear.x = sgn(distance)*g_move_speed;
+    while(timer<final_time) {
+          g_twist_commander.publish(g_twist_cmd);
+          timer+=g_sample_dt;
+          loop_timer.sleep(); 
+          }  
+    do_halt();
+}
+
+void do_halt() {
+    ros::Rate loop_timer(1/g_sample_dt);   
+    g_twist_cmd.angular.z= 0.0;
+    g_twist_cmd.linear.x=0.0;
+    for (int i=0;i<10;i++) {
+          g_twist_commander.publish(g_twist_cmd);
+          loop_timer.sleep(); 
+          }   
+}
+
+class MyActionServer {
 private:
 
     ros::NodeHandle nh_;  // we'll need a node handle; get one upon instantiation
@@ -32,23 +128,24 @@ private:
     // this class will own a "SimpleActionServer" called "as_".
     // it will communicate using messages defined in example_action_server/action/demo.action
     // the type "demoAction" is auto-generated from our name "demo" and generic name "Action"
-    actionlib::SimpleActionServer<example_action_server::demoAction> as_;
+    actionlib::SimpleActionServer<my_action_server::demoAction> as_;
     
     // here are some message types to communicate with our client(s)
-    example_action_server::demoGoal goal_; // goal message, received from client
-    example_action_server::demoResult result_; // put results here, to be sent back to the client when done w/ goal
-    example_action_server::demoFeedback feedback_; // for feedback 
+    my_action_server::myGoal goal_; // goal message, received from client
+    my_action_server::myResult result_; // put results here, to be sent back to the client when done w/ goal
+    my_action_server::myFeedback feedback_; // for feedback 
     //  use: as_.publishFeedback(feedback_); to send incremental feedback to the client
-    int countdown_val_;
+    geometry_msgs::PoseStamped pose_stamped;
+    geometry_msgs::Pose pose;
 
 
 public:
-    ExampleActionServer(); //define the body of the constructor outside of class definition
+    MyActionServer(); //define the body of the constructor outside of class definition
 
-    ~ExampleActionServer(void) {
+    ~MyActionServer(void) {
     }
     // Action Interface
-    void executeCB(const actionlib::SimpleActionServer<example_action_server::demoAction>::GoalConstPtr& goal);
+    void executeCB(const actionlib::SimpleActionServer<my_action_server::demoAction>::GoalConstPtr& goal);
 };
 
 //implementation of the constructor:
@@ -62,12 +159,12 @@ public:
 // using the "this" keyword.  the _1 argument says that our executeCB function takes one argument
 // The final argument,  "false", says don't start the server yet.  (We'll do this in the constructor)
 
-ExampleActionServer::ExampleActionServer() :
-   as_(nh_, "my_action", boost::bind(&ExampleActionServer::executeCB, this, _1),false) 
+MyActionServer::MyActionServer() :
+   as_(nh_, "my_action", boost::bind(&MyActionServer::executeCB, this, _1),false) 
 // in the above initialization, we name the server "example_action"
 //  clients will need to refer to this name to connect with this server
 {
-    ROS_INFO("in constructor of exampleActionServer...");
+    ROS_INFO("in constructor of myActionServer...");
     // do any other desired initializations here...specific to your implementation
 
     as_.start(); //start the server running
@@ -81,41 +178,99 @@ ExampleActionServer::ExampleActionServer() :
 // defined in our package, "example_action_server", in the subdirectory "action", called "demo.action"
 // The name "demo" is prepended to other message types created automatically during compilation.
 // e.g.,  "demoAction" is auto-generated from (our) base name "demo" and generic name "Action"
-void ExampleActionServer::executeCB(const actionlib::SimpleActionServer<example_action_server::demoAction>::GoalConstPtr& goal) {
+void MyActionServer::executeCB(const actionlib::SimpleActionServer<my_action_server::myAction>::GoalConstPtr& goal) {
     ROS_INFO("in executeCB");
     ROS_INFO("goal input is: %d", goal->input);
     //do work here: this is where your interesting code goes
     ros::Rate timer(1.0); // 1Hz timer
-    countdown_val_ = goal->input;
-    //implement a simple timer, which counts down from provided countdown_val to 0, in seconds
-    while (countdown_val_>0) {
-       ROS_INFO("countdown = %d",countdown_val_);
-       
-       // each iteration, check if cancellation has been ordered
-       if (as_.isPreemptRequested()){	
-          ROS_WARN("goal cancelled!");
-          result_.output = countdown_val_;
-          as_.setAborted(result_); // tell the client we have given up on this goal; send the result message as well
-          return; // done with callback
- 		}
+    pose_stamped = goal->input;
+    
+    for (int i=0;i<npts;i++) { //visit each subgoal
+        // odd notation: drill down, access vector element, drill some more to get pose
+        pose_desired = request.nav_path.poses[i].pose; //get next pose from vector of poses
+        
+        //WRITE THIS FNC: compute desired heading and travel distance based on current and desired poses
+        get_yaw_and_dist(g_current_pose, pose_desired,travel_distance, yaw_desired);
+        ROS_INFO("pose %d: desired yaw = %f; desired (x,y) = (%f,%f)",i,yaw_desired,
+          pose_desired.position.x,pose_desired.position.y); 
+        ROS_INFO("current (x,y) = (%f, %f)",g_current_pose.position.x,g_current_pose.position.y);
+        ROS_INFO("travel distance = %f",travel_distance);         
+        
+        
+        // a quaternion is overkill for navigation in a plane; really only need a heading angle
+        // this yaw is measured CCW from x-axis
+        // GET RID OF NEXT LINE AFTER FIXING get_yaw_and_dist()
+        //yaw_desired = convertPlanarQuat2Phi(pose_desired.orientation); //from i'th desired pose
+
+        //get_yaw_and_dist(g_current_pose, pose_desired, travel_distance, yaw_desired);
+        
+        ROS_INFO("pose %d: desired yaw = %f, distance to travel= %f",i,yaw_desired, travel_distance);        
+        yaw_current = convertPlanarQuat2Phi(g_current_pose.orientation); //our current yaw--should use a sensor
+        spin_angle = yaw_desired - yaw_current; // spin this much
+        spin_angle = min_spin(spin_angle);// but what if this angle is > pi?  then go the other way
+        do_spin(spin_angle); // carry out this incremental action
+        // we will just assume that this action was successful--really should have sensor feedback here
+        g_current_pose.orientation = pose_desired.orientation; // assumes got to desired orientation precisely
+        g_current_pose.position = pose_desired.position;
+        
+        //FIX THE NEXT LINE, BASED ON get_yaw_and_dist()
+        do_move(travel_distance);  // move forward 1m...just for illustration; SHOULD compute this from subgoal pose
+        ROS_INFO("current position: %f , %f", g_current_pose.position.x, g_current_pose.position.y);
+        }
  	
  	   //if here, then goal is still valid; provide some feedback
- 	   feedback_.fdbk = countdown_val_; // populate feedback message with current countdown value
+ 	   feedback_.fdbk = pose_stamped; // populate feedback message with current countdown value
  	   as_.publishFeedback(feedback_); // send feedback to the action client that requested this goal
-       countdown_val_--; //decrement the timer countdown
-       timer.sleep(); //wait 1 sec between loop iterations of this timer
     }
     //if we survive to here, then the goal was successfully accomplished; inform the client
-    result_.output = countdown_val_; //value should be zero, if completed countdown
+    result_.output = pose_stamped.pose; //value should be zero, if completed countdown
     as_.setSucceeded(result_); // return the "result" message to client, along with "success" status
 }
 
+void get_yaw_and_dist(geometry_msgs::Pose current_pose, geometry_msgs::Pose goal_pose,double &dist, double &heading) {
+ 
+  int distancex = goal_pose.position.x - current_pose.position.x;
+  int distancey = goal_pose.position.y - current_pose.position.y;
+
+  dist = sqrt(distancex*distancex + distancey*distancey);
+
+  if (dist < g_dist_tol) { //too small of a motion, so just set the heading from goal heading
+    heading = convertPlanarQuat2Phi(goal_pose.orientation); 
+  }
+  else {
+    heading = atan2(distancey, distancex);
+  }
+
+}
+
+void do_inits(ros::NodeHandle &n) {
+  //initialize components of the twist command global variable
+    g_twist_cmd.linear.x=0.0;
+    g_twist_cmd.linear.y=0.0;    
+    g_twist_cmd.linear.z=0.0;
+    g_twist_cmd.angular.x=0.0;
+    g_twist_cmd.angular.y=0.0;
+    g_twist_cmd.angular.z=0.0;  
+    
+    //define initial position to be 0
+    g_current_pose.position.x = 0.0;
+    g_current_pose.position.y = 0.0;
+    g_current_pose.position.z = 0.0;
+    
+    // define initial heading to be "0"
+    g_current_pose.orientation.x = 0.0;
+    g_current_pose.orientation.y = 0.0;
+    g_current_pose.orientation.z = 0.0;
+    g_current_pose.orientation.w = 1.0;
+    
+    // we declared g_twist_commander as global, but never set it up; do that now that we have a node handle
+    g_twist_commander = n.advertise<geometry_msgs::Twist>("/robot0/cmd_vel", 1);    
+}
+
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "timer_action_server_node"); // name this node 
+    ros::init(argc, argv, "my_action_server"); // name this node 
 
-    ROS_INFO("instantiating the timer_action_server: ");
-
-    ExampleActionServer as_object; // create an instance of the class "ExampleActionServer"
+    MyActionServer as_object; // create an instance of the class "ExampleActionServer"
     
     ROS_INFO("going into spin");
     // from here, all the work is done in the action server, with the interesting stuff done within "executeCB()"
